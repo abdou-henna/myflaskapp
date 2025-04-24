997d5a75c27d7d12a0032c8bddf2a661c35545b7
Henna Abderrahmane <henna1dz@gmail.com>
2025-04-24 10:51:18 +0100

    edit test3

--- init_db.py
+++ init_db.py
@@ -1,10 +1,17 @@
-# init_db.py
 from app import get_connection
 import time
 
-time.sleep(5)
+# نحاول الاتصال عدة مرات لتجنب crash لو كانت قاعدة البيانات تتأخر
+for i in range(10):
+    try:
+        conn = get_connection()
+        break
+    except Exception as e:
+        print(f"Waiting for db... ({i+1}/10)")
+        time.sleep(3)
+else:
+    raise Exception("Database connection failed after 10 attempts")
 
-conn = get_connection()
 cur = conn.cursor()
 
 cur.execute("""
